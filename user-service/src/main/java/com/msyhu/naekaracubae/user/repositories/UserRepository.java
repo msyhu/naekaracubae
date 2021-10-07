bd9e528964a166de46d1cdcddba7a7d6b05e1dd3
msyhu <msyhu@korea.ac.kr>
2021-10-07 11:31:54 +0900

    backend email duplicate logic 추가

--- user-service/src/main/java/com/msyhu/naekaracubae/user/repositories/UserRepository.java
+++ user-service/src/main/java/com/msyhu/naekaracubae/user/repositories/UserRepository.java
@@ -4,4 +4,5 @@ import com.msyhu.naekaracubae.user.models.User;
 import org.springframework.data.repository.CrudRepository;
 
 public interface UserRepository extends CrudRepository<User, Long> {
+    boolean existsByEmail(String email);
 }
